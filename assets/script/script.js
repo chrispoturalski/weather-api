95f3ff2b4fed55e0cfc01def69a8c42a795ed741
Chris Poturalski <ChrisPoturalski@chriss-mbp.lan>
2022-11-07 13:30:56 -0800

    added history funtions and for loops, serach history local storage, and weatehr icons

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,6 +9,31 @@ var curCity = document.getElementById('current-city');
 var forecast = document.getElementById('forecast');
 var colRight = document.getElementById('column-right');
 var prevCity = document.getElementById('history');
+var input = document.querySelector('#input');
+
+input.addEventListener('keyup', function(event){
+    if(event.key === 'enter') {
+        createWeatherDisplay(event.target.value)
+    }
+})
+
+var previousSearchHistory = localStorage.getItem('history')
+if (previousSearchHistory) {
+    previousSearchHistory = JSON.parse(previousSearchHistory)
+} else {
+    previousSearchHistory = []
+}
+
+for (var i = 0; i < previousSearchHistory.length; i++) {
+    var historyBtn = document.createElement('button')
+    var historyItem = previousSearchHistory[i]
+    historyBtn.textContent = historyItem
+    historyBtn.addEventListener('click', function(event) {
+        createWeatherDisplay(event.target.textContent)
+    })
+}
+
+
 
 //function to grab location from API Key
 function getGeoLocation(query, limit = 5){
@@ -18,24 +43,49 @@ function getGeoLocation(query, limit = 5){
 function getCurrentWeather({arguments}) {
     return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${arguments.lat}&lon=${arguments.lon}&units=${'imperial'}&appid={API_KEY}`)
 }
-
-
-getGeoLocation('Irvine')
-.then(response => response.json())
-.then(data => {
-    var { lat, lon } = data[0]
-    getCurrentWeather ({lat, lon})
-    .then(weatherResponse => weatherResponse.json())
-    .then(weatherData => {
-        console.log(JSON.stringify(data, null, 2))
+//A function that returns a promise. The promise is a fetch to api geolocator
+function createWeatherDisplay(location){
+    var searchHistory = localStorage.getItem('history')
+    if (searchHistory) {
+        searchHistory = JSON.parse(searchHistory)
+        searchHistory.push(location)
+        localStorage.setItem('history', JSON.stringify(searchHistory))
+    } else {
+        searchHistory = (location)
+        localStorage.setItem('history', JSON.stringify(searchHistory))
+    }
+    getGeoLocation('Irvine')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(data => {
+        if (data.length === 0) {
+            var errorEl = document.createElement('p')
+            errorEl.textContent = `We couldn't find ${location}`
+            document.body.appendChild(errorEl)
+        } else {
+            getCurrentWeather ({ lat: data[0].lat, lon: data[0].lon })
+            .then(weatherResponse => weatherResponse.json())
+            .then(weatherData => {
+                //This following function will display the weather icon and basic despription of current weather
+                var weatherPicture = document.createElement('img')
+                weatherPicture.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+                var currentWeatherStatement = document.createElement('p')
+                currentWeatherStatement.textContent = `${weatherData.weather[0].main}: it is currently ${weatherData.weather[0].description}`
+                document.body.appendChild(weatherPicture)
+                document.appendChild(currentWeatherStatement)
+                console.log(JSON.stringify(data, null, 2))
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+        }
     })
     .catch(error => {
         console.log(error.message)
-    })
-})
-.catch(error => {
-    console.log(error.message)
-});
+    });
+}
+
 //Criteria
 // I want to see the weather outlook for multiple cities
 // GIVEN a weather dashboard with form input
